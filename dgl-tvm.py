cdd9d9bbf3935b8754f4bf6911f61f9702b19137
Ubuntu <ubuntu@ip-172-31-2-188.us-east-2.compute.internal>
2020-07-09 08:28:07 +0000

    verify spmm

--- dgl-tvm.py
+++ dgl-tvm.py
@@ -3,14 +3,15 @@ import dgl.sparse
 import torch as th
 from utils import th_op_time, get_graph
 from featgraph.module import gsddmm, gspmm
-from featgraph.util import calc_bcast
+from featgraph.util import calc_bcast, partition_csr
 import argparse
 import tvm
 from tvm.contrib.dlpack import to_pytorch_func
 import numpy as np
+import util
 
-n_cold_start = 5
-num_runs = 20
+n_cold_start = 2
+num_runs = 10
 binary_op_map = {
     'add': lambda x,y : x+y,
     'sub': lambda x,y : x-y,
@@ -36,10 +37,10 @@ def bench_spmm(g, target, ctx, th_ctx, generic=False):
     tvm_indptr = tvm.nd.array(adj_indptr, ctx=ctx)
     tvm_indices = tvm.nd.array(adj_indices, ctx=ctx)
     def measure_time(binary_op, reduce_op, 
-                     node_feat_shape, edge_feat_shape, feat_type, 
+                     src_feat_shape, dst_feat_shape, feat_type, 
                      num_warmup, num_runs, verify=False):
         use_bcast, lhs_len, rhs_len, out_len, _, lhs_off, rhs_off = \
-            calc_bcast(binary_op, np.zeros((1,)+node_feat_shape), np.zeros((1,)+edge_feat_shape))
+            calc_bcast(binary_op, np.zeros((1,)+src_feat_shape), np.zeros((1,)+dst_feat_shape))
         if generic:
             f = tvm.build(gspmm.gspmm(binary_op, reduce_op, id_type, feat_type,
                                       use_bcast=use_bcast, target=target), target=target)
@@ -51,20 +52,20 @@ def bench_spmm(g, target, ctx, th_ctx, generic=False):
                 use_bcast=use_bcast, target=target
             )
         # try:
-        use_node, use_edge = True, True
+        use_src, use_dst = True, True
         if binary_op == 'copy_u':
-            use_edge = False
+            use_dst = False
         elif binary_op == 'copy_e':
-            use_node = False
+            use_src = False
         f_input = [tvm_indptr, tvm_indices]
-        if use_node:
-            node_feat = th.rand((num_cols, lhs_len), dtype=th_dtype_mapping[feat_type], device=th_ctx)
-            tvm_node_feat = tvm.nd.from_dlpack(th.utils.dlpack.to_dlpack(node_feat))
-            f_input.append(tvm_node_feat)
-        if use_edge:
-            edge_feat = th.rand((nnz, rhs_len), dtype=th_dtype_mapping[feat_type], device=th_ctx)
-            tvm_edge_feat = tvm.nd.from_dlpack(th.utils.dlpack.to_dlpack(edge_feat))
-            f_input.append(tvm_edge_feat)
+        if use_src:
+            src_feat = th.rand((num_cols, lhs_len), dtype=th_dtype_mapping[feat_type], device=th_ctx)
+            tvm_src_feat = tvm.nd.from_dlpack(th.utils.dlpack.to_dlpack(src_feat))
+            f_input.append(tvm_src_feat)
+        if use_dst:
+            dst_feat = th.rand((nnz, rhs_len), dtype=th_dtype_mapping[feat_type], device=th_ctx)
+            tvm_dst_feat = tvm.nd.from_dlpack(th.utils.dlpack.to_dlpack(dst_feat))
+            f_input.append(tvm_dst_feat)
         if use_bcast:
             tvm_lhs_off = tvm.nd.array(lhs_off.astype(id_type), ctx=ctx)
             tvm_rhs_off = tvm.nd.array(rhs_off.astype(id_type), ctx=ctx)
@@ -73,8 +74,11 @@ def bench_spmm(g, target, ctx, th_ctx, generic=False):
         tvm_out = tvm.nd.from_dlpack(th.utils.dlpack.to_dlpack(out))
         f_input.append(tvm_out)
         # verify result is correct
-        if verify:
-            print('Not implemented')
+        if verify and binary_op == 'mul' and reduce_op == 'sum' and out_len == 1:
+            f(*f_input)
+            adj_scipy_csr.data = dst_feat.cpu().numpy()
+            np_result = adj_scipy_csr.dot(src_feat.cpu().numpy())
+            np.testing.assert_allclose(np_result, tvm_out.asnumpy(), rtol=1e-4, atol=1e-4)
         else:
             # warmup
             for _ in range(num_warmup):
@@ -82,24 +86,38 @@ def bench_spmm(g, target, ctx, th_ctx, generic=False):
             # measure average time
             timer = f.time_evaluator(f.entry_name, ctx=ctx, number=num_runs)
             tcost = timer(*f_input).mean
-            print('binary_op: {}\treduce_op:{}\tnode_feat_shape: {}\tedge_feat_shape: {}\telpased time: {:.3e}s'
-                .format(binary_op, reduce_op, node_feat_shape, edge_feat_shape, tcost))
+            print('binary_op: {}\treduce_op:{}\tsrc_feat_shape: {}\tdst_feat_shape: {}\telpased time: {:.3e}s'
+                .format(binary_op, reduce_op, src_feat_shape, dst_feat_shape, tcost))
         # except Exception as e:
         #     print(e)
-    shapes = [(2 ** x,) for x in range(8)]
+    shapes = [(2 ** x,) for x in range(3)]
     for shape in shapes:
-        measure_time('copy_u', 'sum', shape, shape, 'float32', n_cold_start, num_runs)
-    for shape in shapes:
-        measure_time('copy_u', 'max', shape, shape, 'float32', n_cold_start, num_runs)
+        measure_time('mul', 'sum', shape, shape, 'float32', n_cold_start, num_runs, verify=True)
+    # for shape in shapes:
+    #     measure_time('copy_u', 'max', shape, shape, 'float32', n_cold_start, num_runs)
     
-def bench_sddmm(g, target, ctx, th_ctx, generic=False):
+def bench_sddmm(g, target, ctx, th_ctx, generic=False, 
+                partition=True):
     print("SDDMM\n----------------------------")
-    adj_scipy_coo = g.adjacency_matrix(scipy_fmt='coo')
-    adj_row_indices = adj_scipy_coo.row
-    adj_col_indices = adj_scipy_coo.col
-    num_rows, num_cols = adj_scipy_coo.shape[0], adj_scipy_coo.shape[1]
-    nnz = adj_row_indices.shape[0]
-    id_type = str(adj_row_indices.dtype)
+    if not partition:
+        adj_scipy_coo = g.adjacency_matrix(scipy_fmt='coo')
+        num_rows, num_cols = adj_scipy_coo.shape[0], adj_scipy_coo.shape[1]
+        adj_row_indices = adj_scipy_coo.row
+        adj_col_indices = adj_scipy_coo.col
+        nnz = adj_row_indices.shape[0]
+        id_type = str(adj_row_indices.dtype)
+    else:
+        adj_scipy_csr = g.adjacency_matrix(scipy_fmt='csr')
+        num_rows, num_cols = adj_scipy_csr.shape[0], adj_scipy_csr.shape[1]
+        nnz = adj_scipy_csr.indices.shape[0]
+        id_type = str(adj_scipy_csr.indices.dtype)
+        adj_row_indices = np.zeros(shape=(nnz,), dtype=id_type)
+        adj_col_indices = np.zeros(shape=(nnz,), dtype=id_type)
+        edge_id = np.arange(1, 1+nnz, dtype=id_type)
+        par_edge_id = np.zeros(shape=(nnz,), dtype=id_type)
+        util.partition_2d(adj_scipy_csr.indptr, adj_scipy_csr.indices, edge_id,
+                       adj_row_indices, adj_col_indices, par_edge_id,
+                       num_rows, num_cols, 32, 32)
     tvm_row_indices = tvm.nd.array(adj_row_indices, ctx=ctx)
     tvm_col_indices = tvm.nd.array(adj_col_indices, ctx=ctx)
     def measure_time(binary_op, src_feat_shape, dst_feat_shape, 
@@ -141,8 +159,8 @@ def bench_sddmm(g, target, ctx, th_ctx, generic=False):
             # verify result is correct
             if verify:
                 f(*f_input)
-                lhs = src_feat[adj_scipy_coo.row]
-                rhs = dst_feat[adj_scipy_coo.col]
+                lhs = src_feat[adj_row_indices]
+                rhs = dst_feat[adj_col_indices]
                 if binary_op != 'dot':
                     np_result = binary_op_map[binary_op](lhs, rhs)
                 else:
@@ -152,7 +170,7 @@ def bench_sddmm(g, target, ctx, th_ctx, generic=False):
                         lhs = lhs.reshape((lhs.shape[0], feat_len // reduce_size, reduce_size))
                         rhs = rhs.reshape((rhs.shape[0], feat_len // reduce_size, reduce_size))
                         np_result = (lhs * rhs).sum(axis=-1)
-                if th_ctx.type == 'cuda':
+                if target == 'cuda':
                     np_result = np_result.cpu()
                 np.testing.assert_allclose(np_result, tvm_out.asnumpy(), rtol=1e-4, atol=1e-4)
             else:
@@ -169,8 +187,8 @@ def bench_sddmm(g, target, ctx, th_ctx, generic=False):
     shapes = [(2 ** x,) for x in range(8)]
     for shape in shapes:
         measure_time('add', shape, shape, 'float32', n_cold_start, num_runs)
-    for shape in shapes:
-        measure_time('dot', shape, shape, 'float32', n_cold_start, num_runs)
+    # for shape in shapes:
+    #     measure_time('dot', shape, shape, 'float32', n_cold_start, num_runs)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Benchmark DGL kernels")
@@ -184,10 +202,12 @@ if __name__ == '__main__':
         target = 'cuda'
         ctx = th.device(int(args.gpu))
         tvm_ctx = tvm.gpu(int(args.gpu))
-    for dataset in ['arxiv', 'reddit', 'proteins']:
+    # for dataset in ['arxiv', 'reddit', 'proteins']:
+    for dataset in ['arxiv']:
         g = get_graph(dataset)
-        print(dataset)
+        print(g)
         # SPMM
         bench_spmm(g, target, tvm_ctx, ctx, generic=False)
         # SDDMM
-        # bench_sddmm(g, target, tvm_ctx, ctx, generic=False)
+        # bench_sddmm(g, target, tvm_ctx, ctx, generic=False, partition=True)
+        
