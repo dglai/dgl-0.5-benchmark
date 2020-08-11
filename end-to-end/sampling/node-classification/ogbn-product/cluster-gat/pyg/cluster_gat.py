1ceb23f02315b63a74aaf967dd5094803cbf6532
yzh119 <expye@outlook.com>
2020-08-11 14:42:06 +0800

    upd

---
+++ end-to-end/sampling/node-classification/ogbn-product/cluster-gat/pyg/cluster_gat.py
@@ -0,0 +1,238 @@
+import argparse
+import time
+
+import torch
+from tqdm import tqdm
+import torch.nn.functional as F
+from torch.nn import Linear as Lin
+from tqdm import tqdm
+
+from torch_geometric.data import ClusterData, ClusterLoader, NeighborSampler
+from torch_geometric.nn import GATConv
+
+from ogb.nodeproppred import PygNodePropPredDataset, Evaluator
+
+from logger import Logger
+
+class GAT(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers,
+                 heads):
+        super(GAT, self).__init__()
+        print(in_channels)
+        print(hidden_channels)
+        print(out_channels)
+        print(num_layers)
+
+        self.num_layers = num_layers
+
+        self.convs = torch.nn.ModuleList()
+        self.convs.append(GATConv(in_channels, hidden_channels,
+                                  heads))
+        for _ in range(num_layers - 2):
+            self.convs.append(
+                GATConv(heads * hidden_channels, hidden_channels, heads))
+        self.convs.append(
+            GATConv(heads * hidden_channels, out_channels, heads,
+                    concat=False))
+
+        self.skips = torch.nn.ModuleList()
+        self.skips.append(Lin(in_channels, hidden_channels * heads))
+        for _ in range(num_layers - 2):
+            self.skips.append(
+                Lin(hidden_channels * heads, hidden_channels * heads))
+        self.skips.append(Lin(hidden_channels * heads, out_channels))
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+        for skip in self.skips:
+            skip.reset_parameters()
+
+    def forward(self, x_target, edge_index):
+        # `train_loader` computes the k-hop neighborhood of a batch of nodes,
+        # and returns, for each layer, a bipartite graph object, holding the
+        # bipartite edges `edge_index`, the index `e_id` of the original edges,
+        # and the size/shape `size` of the bipartite graph.
+        # Target nodes are also included in the source nodes so that one can
+        # easily apply skip-connections or add self-loops.
+        for i in range(self.num_layers):
+            x = self.convs[i](x_target, edge_index)
+            x = x + self.skips[i](x_target)
+            if i != self.num_layers - 1:
+                x = F.elu(x)
+                x = F.dropout(x, p=0.5, training=self.training)
+            x_target = x
+        return x.log_softmax(dim=-1)
+
+    def inference(self, x_all):
+        pbar = tqdm(total=x_all.size(0) * self.num_layers)
+        pbar.set_description('Evaluating')
+
+        # Compute representations of nodes layer by layer, using *all*
+        # available edges. This leads to faster computation in contrast to
+        # immediately computing the final representations of each batch.
+        total_edges = 0
+        for i in range(self.num_layers):
+            xs = []
+            for batch_size, n_id, adj in subgraph_loader:
+                edge_index, _, size = adj.to(device)
+                total_edges += edge_index.size(1)
+                x = x_all[n_id].to(device)
+                x_target = x[:size[1]]
+                x = self.convs[i]((x, x_target), edge_index)
+                x = x + self.skips[i](x_target)
+
+                if i != self.num_layers - 1:
+                    x = F.elu(x)
+                xs.append(x.cpu())
+
+                pbar.update(batch_size)
+
+            x_all = torch.cat(xs, dim=0)
+
+        pbar.close()
+
+        return x_all
+
+def train(model, loader, optimizer, device):
+    model.train()
+
+    total_loss = total_examples = 0
+    total_correct = total_examples = 0
+    l_t = 0
+    f_t = 0
+    b_t = 0
+    start = time.time()
+    for data in loader:
+        data = data.to(device)
+        if data.train_mask.sum() == 0:
+            continue
+        load = time.time()
+        optimizer.zero_grad()
+        out = model(data.x, data.edge_index)[data.train_mask]
+        y = data.y.squeeze(1)[data.train_mask]
+        loss = F.nll_loss(out, y)
+        forw = time.time()
+        loss.backward()
+        optimizer.step()
+        back = time.time()
+
+        l_t += (load - start)
+        f_t += (forw - load)
+        b_t += (back - forw)
+        start = time.time()
+        num_examples = data.train_mask.sum().item()
+        total_loss += loss.item() * num_examples
+        total_examples += num_examples
+
+        total_correct += out.argmax(dim=-1).eq(y).sum().item()
+        total_examples += y.size(0)
+
+    print('Load {:.4f} Forward {:.4f} Backward {:.4f}'.format(l_t, f_t, b_t))
+    return total_loss / total_examples, total_correct / total_examples
+
+
+@torch.no_grad()
+def test(model, data, evaluator, subgraph_loader, device):
+    model.eval()
+
+    out = model.inference(data.x, subgraph_loader, device)
+
+    y_true = data.y
+    y_pred = out.argmax(dim=-1, keepdim=True)
+
+    train_acc = evaluator.eval({
+        'y_true': y_true[data.train_mask],
+        'y_pred': y_pred[data.train_mask]
+    })['acc']
+    valid_acc = evaluator.eval({
+        'y_true': y_true[data.valid_mask],
+        'y_pred': y_pred[data.valid_mask]
+    })['acc']
+    test_acc = evaluator.eval({
+        'y_true': y_true[data.test_mask],
+        'y_pred': y_pred[data.test_mask]
+    })['acc']
+
+    return train_acc, valid_acc, test_acc
+
+
+def main():
+    parser = argparse.ArgumentParser(description='OGBN-Products (Cluster-GCN)')
+    parser.add_argument('--device', type=int, default=0)
+    parser.add_argument('--log_steps', type=int, default=1)
+    parser.add_argument('--num_partitions', type=int, default=15000)
+    parser.add_argument('--num_workers', type=int, default=8)
+    parser.add_argument('--num_layers', type=int, default=3)
+    parser.add_argument('--hidden_channels', type=int, default=256)
+    parser.add_argument('--dropout', type=float, default=0.5)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--eval_steps', type=int, default=5)
+    parser.add_argument('--runs', type=int, default=10)
+    args = parser.parse_args()
+    print(args)
+
+    device = f'cuda:{args.device}' if torch.cuda.is_available() else 'cpu'
+    device = torch.device(device)
+
+    dataset = PygNodePropPredDataset(name='ogbn-products')
+    split_idx = dataset.get_idx_split()
+    data = dataset[0]
+
+    # Convert split indices to boolean masks and add them to `data`.
+    for key, idx in split_idx.items():
+        mask = torch.zeros(data.num_nodes, dtype=torch.bool)
+        mask[idx] = True
+        data[f'{key}_mask'] = mask
+
+    cluster_data = ClusterData(data, num_parts=args.num_partitions,
+                               recursive=False, save_dir=dataset.processed_dir)
+
+    loader = ClusterLoader(cluster_data, batch_size=args.batch_size,
+                           shuffle=True, num_workers=args.num_workers)
+
+    subgraph_loader = NeighborSampler(data.edge_index, sizes=[-1],
+                                      batch_size=1024, shuffle=False,
+                                      num_workers=args.num_workers)
+
+    model = GAT(data.x.size(-1), args.hidden_channels, dataset.num_classes,
+                args.num_layers, heads=8).to(device)
+
+    evaluator = Evaluator(name='ogbn-products')
+    logger = Logger(args.runs, args)
+
+    for run in range(args.runs):
+        model.reset_parameters()
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+        t1 = time.time()
+        for epoch in range(1, 1 + args.epochs):
+            loss, train_acc = train(model, loader, optimizer, device)
+            t2 = time.time()
+            epoch_t = t2 - t1
+            t1 = time.time()
+            if epoch % args.log_steps == 0:
+                print(f'Run: {run + 1:02d}, '
+                      f'Epoch: {epoch:02d}, '
+                      f'Time: {epoch_t:.4f}, '
+                      f'Loss: {loss:.4f}, '
+                      f'Approx Train Acc: {train_acc:.4f}')
+
+            if epoch > 19 and epoch % args.eval_steps == 0:
+                result = test(model, data, evaluator, subgraph_loader, device)
+                logger.add_result(run, result)
+                train_acc, valid_acc, test_acc = result
+                print(f'Run: {run + 1:02d}, '
+                      f'Epoch: {epoch:02d}, '
+                      f'Time: {epoch_t:.4f}, '
+                      f'Train: {100 * train_acc:.2f}%, '
+                      f'Valid: {100 * valid_acc:.2f}% '
+                      f'Test: {100 * test_acc:.2f}%')
+
+        logger.print_statistics(run)
+    logger.print_statistics()
+
+
+if __name__ == "__main__":
+    main()
