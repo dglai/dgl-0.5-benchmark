e5aca0c4476fe49551832e99f8c9f2dc6056d221
yzh119 <expye@outlook.com>
2020-08-13 14:01:09 +0000

    upd

--- kernel/dgl-new.py
+++ kernel/dgl-new.py
@@ -1,37 +1,32 @@
 import dgl
-import dgl.sparse
+import dgl.ops
 import torch as th
-import csv
 from utils import th_op_time, get_graph
 
 import argparse
 
 n_cold_start = 2
 
-def bench_spmm(csvfile, g, ctx, binary_op, reduce_op):
-    writer = csv.writer(csvfile)
+def bench_spmm(g, ctx, binary_op, reduce_op):
     print("SPMM\n----------------------------")
     with th.no_grad():
         for n_hid in [1, 2, 4, 8, 16, 32, 64, 128]:
             try:
                 nfeat = th.rand(g.number_of_src_nodes(), n_hid, device=ctx)
-                efeat = th.rand(g.number_of_edges(), n_hid, device=ctx) if binary_op != 'copy_u' else None
+                efeat = th.rand(g.number_of_edges(), n_hid, device=ctx) if binary_op != 'copy_lhs' else None
                 accum_time = 0
                 for n_times in range(10):
                     with th_op_time() as timer:
-                        dgl.sparse.gspmm(g, binary_op, reduce_op, nfeat, efeat)
+                        dgl.ops.gspmm(g, binary_op, reduce_op, nfeat, efeat)
                     if n_times >= n_cold_start:
                         accum_time += timer.time
                 avg_time = accum_time / (n_times - n_cold_start)
                 print('hidden size: {}, avg time: {}'.format(
                     n_hid, avg_time))
-                writer.writerow([str(n_hid), str(avg_time)])
             except:
                 print('hidden size: {}, OOM'.format(n_hid))
-                writer.writerow([str(n_hid), 'OOM'])
 
-def bench_sddmm(csvfile, g, ctx, op):
-    writer = csv.writer(csvfile)
+def bench_sddmm(g, ctx, op):
     print("SDDMM\n----------------------------")
     with th.no_grad():
         for n_hid in [1, 2, 4, 8, 16, 32, 64, 128]:
@@ -41,20 +36,18 @@ def bench_sddmm(csvfile, g, ctx, op):
                 accum_time = 0
                 for n_times in range(10):
                     with th_op_time() as timer:
-                        dgl.sparse.gsddmm(g, op, ufeat, vfeat)
+                        dgl.ops.gsddmm(g, op, ufeat, vfeat)
                     if n_times >= n_cold_start:
                         accum_time += timer.time
                 avg_time = accum_time / (n_times - n_cold_start)
                 print('hidden size: {}, avg time: {}'.format(
                     n_hid, avg_time))
-                writer.writerow([str(n_hid), str(avg_time)])
             except:
                 print('hidden size: {}, OOM'.format(n_hid))
-                writer.writerow([str(n_hid), 'OOM'])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Benchmark DGL kernels")
-    parser.add_argument('--spmm-binary', type=str, default='copy_u')
+    parser.add_argument('--spmm-binary', type=str, default='copy_lhs')
     parser.add_argument('--spmm-reduce', type=str, default='sum')
     parser.add_argument('--sddmm-binary', type=str, default='add')
     parser.add_argument('--gpu', '-g', type=str, default='-1')
@@ -67,11 +60,11 @@ if __name__ == '__main__':
 
     for dataset in ['reddit', 'arxiv', 'proteins']:
         g = get_graph(dataset)
+        g = g.int().to(ctx)
         print(g)
         # SPMM
-        with open('_'.join(['new', dataset, 'spmm', ctx_str, args.spmm_binary, args.spmm_reduce]) + '.csv', 'w') as csvfile:
-            bench_spmm(csvfile, g, ctx, args.spmm_binary, args.spmm_reduce)
+        bench_spmm(g, ctx, args.spmm_binary, args.spmm_reduce)
         # SDDMM
         if ctx_str == 'cpu': continue  # sddmm out of mem on cpu will result in termination of the program.
-        with open('_'.join(['new', dataset, 'sddmm', ctx_str, args.sddmm_binary]) + '.csv', 'w') as csvfile:
-            bench_sddmm(csvfile, g, ctx, args.sddmm_binary)
+        bench_sddmm(g, ctx, args.sddmm_binary)
+        del g
