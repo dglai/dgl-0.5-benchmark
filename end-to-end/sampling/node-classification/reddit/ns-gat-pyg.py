40013de3e1975c470eae2fe4710e10405ae5bc61
yzh119 <expye@outlook.com>
2020-08-25 18:08:06 +0800

    upd

---
+++ end-to-end/sampling/node-classification/reddit/ns-gat-pyg.py
@@ -0,0 +1,163 @@
+import os.path as osp
+
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
+from torch_geometric.datasets import Reddit
+from torch_geometric.data import NeighborSampler
+import time
+from torch_geometric.nn import SAGEConv
+from torch_geometric.nn import GATConv
+from torch.nn import Linear as Lin
+
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'Reddit')
+dataset = Reddit(path)
+data = dataset[0]
+
+train_loader = NeighborSampler(data.edge_index, node_idx=data.train_mask,
+                               sizes=[25, 10], batch_size=1024, shuffle=True,
+                               num_workers=8)
+subgraph_loader = NeighborSampler(data.edge_index, node_idx=None, sizes=[-1],
+                                  batch_size=1024, shuffle=False,
+                                  num_workers=8)
+
+class GAT(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers,
+                 heads):
+        super(GAT, self).__init__()
+
+        self.num_layers = num_layers
+
+        self.convs = torch.nn.ModuleList()
+        self.convs.append(GATConv(dataset.num_features, hidden_channels,
+                                  heads))
+        for _ in range(num_layers - 2):
+            self.convs.append(
+                GATConv(heads * hidden_channels, hidden_channels, heads))
+        self.convs.append(
+            GATConv(heads * hidden_channels, out_channels, heads,
+                    concat=False))
+
+        self.skips = torch.nn.ModuleList()
+        self.skips.append(Lin(dataset.num_features, hidden_channels * heads))
+        for _ in range(num_layers - 2):
+            self.skips.append(
+                Lin(hidden_channels * heads, hidden_channels * heads))
+        self.skips.append(Lin(hidden_channels * heads, out_channels))
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+        for skip in self.skips:
+            skip.reset_parameters()
+
+    def forward(self, x, adjs):
+        # `train_loader` computes the k-hop neighborhood of a batch of nodes,
+        # and returns, for each layer, a bipartite graph object, holding the
+        # bipartite edges `edge_index`, the index `e_id` of the original edges,
+        # and the size/shape `size` of the bipartite graph.
+        # Target nodes are also included in the source nodes so that one can
+        # easily apply skip-connections or add self-loops.
+        for i, (edge_index, _, size) in enumerate(adjs):
+            x_target = x[:size[1]]  # Target nodes are always placed first.
+            x = self.convs[i]((x, x_target), edge_index)
+            x = x + self.skips[i](x_target)
+            if i != self.num_layers - 1:
+                x = F.elu(x)
+                x = F.dropout(x, p=0.5, training=self.training)
+        return x.log_softmax(dim=-1)
+
+    def inference(self, x_all):
+        pbar = tqdm(total=x_all.size(0) * self.num_layers)
+        pbar.set_description('Evaluating')
+
+        # Compute representations of nodes layer by layer, using *all*
+        # available edges. This leads to faster computation in contrast to
+        # immediately computing the final representations of each batch.
+        total_edges = 0
+        for i in range(self.num_layers):
+            xs = []
+            for batch_size, n_id, adj in subgraph_loader:
+                edge_index, _, size = adj.to(device)
+                total_edges += edge_index.size(1)
+                x = x_all[n_id].to(device)
+                x_target = x[:size[1]]
+                x = self.convs[i]((x, x_target), edge_index)
+                x = x + self.skips[i](x_target)
+
+                if i != self.num_layers - 1:
+                    x = F.elu(x)
+                xs.append(x.cpu())
+
+                pbar.update(batch_size)
+
+            x_all = torch.cat(xs, dim=0)
+
+        pbar.close()
+
+        return x_all
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = GAT(dataset.num_features, 128, dataset.num_classes, 2, 8)
+model = model.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+x = data.x#.to(device)
+y = data.y.squeeze()#.to(device)
+
+
+def train(epoch):
+    model.train()
+
+
+    total_loss = total_correct = 0
+    t = time.time()
+    t_load = 0
+    t_s = time.time()
+    for batch_size, n_id, adjs in train_loader:
+        # `adjs` holds a list of `(edge_index, e_id, size)` tuples.
+        adjs = [adj.to(device) for adj in adjs]
+        feat = x[n_id].to(device)
+        label = y[n_id[:batch_size]].to(device)
+        t_l = time.time()
+
+        optimizer.zero_grad()
+        out = model(feat, adjs)
+        loss = F.nll_loss(out, label)
+        loss.backward()
+        optimizer.step()
+
+        total_loss += float(loss)
+        t_load += (t_l - t_s)
+        t_s = time.time()
+
+    print('Epoch {}'.format(time.time() -t))
+    print('Load {}'.format(t_load))
+    loss = total_loss / len(train_loader)
+    approx_acc = total_correct / int(data.train_mask.sum())
+
+    return loss, approx_acc
+
+
+@torch.no_grad()
+def test():
+    model.eval()
+
+    out = model.inference(x)
+
+    y_true = y.cpu().unsqueeze(-1)
+    y_pred = out.argmax(dim=-1, keepdim=True)
+
+    results = []
+    for mask in [data.train_mask, data.val_mask, data.test_mask]:
+        results += [int(y_pred[mask].eq(y_true[mask]).sum()) / int(mask.sum())]
+
+    return results
+
+
+for epoch in range(1, 11):
+    loss, acc = train(epoch)
+    print(f'Epoch {epoch:02d}, Loss: {loss:.4f}, Approx. Train: {acc:.4f}')
+    train_acc, val_acc, test_acc = test()
+    print(f'Train: {train_acc:.4f}, Val: {val_acc:.4f}, '
+          f'Test: {test_acc:.4f}')
